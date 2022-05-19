ac27fd537e91ac85ccd15a21eb2c2743a1a0dec8
sergio1023 <sergyfutbol37@gmail.com>
2022-05-19 18:07:40 +0200

    base de datos y error en concatenación

--- profeSergioMuñoz.py
+++ profeSergioMuñoz.py
@@ -7,7 +7,7 @@ nombre = input("Nombre y apellidos del alumno: ")
 pregunta1 = input("¿Quieres hacer un examen o añadir un nuevo tema?")
 
 if  pregunta1 == "examen":
-		print("Elige entre examen de moda, coches, redes de internet o historia de españa")
+		print("Elige entre examen de hardware, coches, redes de internet o historia de españa")
 		print("Preparando examen para ") nombre
 	else:
 		 if ( pregunta1 = "nuevo test")
@@ -15,3 +15,4 @@ if  pregunta1 == "examen":
 
 # Página respuestas historia https://escuelapce.com/test/historia-de-espana/
 # Página respuestas redes https://cibertest.com/examen-online/580/redes-basico
+# Página de respuestas hardware https://cibertest.com/examen-online/2324/partes-de-una-computadora
