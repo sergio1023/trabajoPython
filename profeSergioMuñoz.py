5fc823c73f75466459d23edabdd6dd39e114cfb8
sergio1023 <sergyfutbol37@gmail.com>
2022-05-24 13:22:12 +0200

    meter archivos sql

--- profeSergioMuñoz.py
+++ profeSergioMuñoz.py
@@ -3,16 +3,18 @@
 
 import os
 
-nombre = input("Nombre y apellidos del alumno: ")
+nombre = input("Nombre del alumno: ")
+print("Contesta examen o nuevo test")
 pregunta1 = input("¿Quieres hacer un examen o añadir un nuevo tema?")
 
 if  pregunta1 == "examen":
 		print("Elige entre examen de hardware, coches, redes de internet o historia de españa")
 		print("Preparando examen para ") nombre
 	else:
-		 if ( pregunta1 = "nuevo test")
+		 if  pregunta1 == "nuevo test":
 
 
 # Página respuestas historia https://escuelapce.com/test/historia-de-espana/
+#De la pregunta 11 a la 12 https://buen-saber.com/quiz/historia-de-espana/
 # Página respuestas redes https://cibertest.com/examen-online/580/redes-basico
-# Página de respuestas hardware https://cibertest.com/examen-online/2324/partes-de-una-computadora
+# Página respuestas hardware https://cibertest.com/examen-online/2324/partes-de-una-computadora
